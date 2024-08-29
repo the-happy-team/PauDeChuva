f9392bd527ddd7a91d72af07b1596182092e25d2
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-08-28 20:00:12 -0400

    update motor rotation sequence

--- PauDeChuva.ino
+++ PauDeChuva.ino
@@ -1,30 +1,25 @@
 #include "PauDeChuva.h"
 
-PauDeChuva mPDC[3] = { { 4, 3, 2, 5 }, { 8, 7, 6, 9 }, { 12, 11, 10, 13 } };
+PauDeChuva mPDC[3] = { { 4, 3, 2, 5, 6400 }, { 8, 7, 6, 9, 6400 }, { 12, 11, 10, 13, 6400 } };
 
 void setup() {
+  mPDC[0].addRotation(0, 0);
   mPDC[0].addRotation(0, 2);
-  mPDC[0].addRotation(180, 30);
-  mPDC[0].addRotation(0, 20);
-  mPDC[0].addRotation(0, 8);
+  mPDC[0].addRotation(180, 45);
+  mPDC[0].addRotation(0, 5);
+  mPDC[0].addRotation(0, 53);
 
-  mPDC[1].addRotation(0, 2);
-  mPDC[1].addRotation(90, 30);
-  mPDC[1].addRotation(0, 15);
-  mPDC[1].addRotation(90, 30);
-  mPDC[1].addRotation(0, 20);
-  mPDC[1].addRotation(0, 8);
+  mPDC[1].addRotation(0, 0);
+  mPDC[1].addRotation(0, 11);
+  mPDC[1].addRotation(180, 45);
+  mPDC[1].addRotation(0, 5);
+  mPDC[1].addRotation(0, 44);
 
-  mPDC[2].addRotation(0, 2);
-  mPDC[2].addRotation(45, 20);
-  mPDC[2].addRotation(0, 10);
-  mPDC[2].addRotation(45, 20);
-  mPDC[2].addRotation(0, 10);
-  mPDC[2].addRotation(45, 20);
-  mPDC[2].addRotation(0, 10);
-  mPDC[2].addRotation(45, 20);
+  mPDC[2].addRotation(0, 0);
   mPDC[2].addRotation(0, 20);
-  mPDC[2].addRotation(0, 8);
+  mPDC[2].addRotation(180, 45);
+  mPDC[2].addRotation(0, 5);
+  mPDC[2].addRotation(0, 35);
 }
 
 void loop() {
